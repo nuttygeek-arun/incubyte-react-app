48f4bfa6ee5e781b7a722d6e0fc47953f26de4cb
Arun Sharma <nuttygeek.arun@gmail.com>
2025-03-10 14:08:19 +0530

    should render sum of provided input

--- incubyte-react-app/src/App.test.js
+++ incubyte-react-app/src/App.test.js
@@ -12,3 +12,18 @@ test('renders calculate on the screen', () => {
   const calculateButton = screen.getByTestId('calculate');
   expect(calculateButton).toBeInTheDocument();
 });
+
+test('should render sum of provided input', () => {
+  render(<App />);
+  // getting input element
+  const inputWrapper = screen.getByTestId("input");
+  const inputElement = inputWrapper.querySelector("input");
+  // providing value to input element
+  fireEvent.change(inputElement, { target: { value: "1,2,3" } });
+  // getting calculate button
+  const calculateButton = screen.getByTestId('calculate');
+  // triggering click on calculate button
+  fireEvent.click(calculateButton);
+  const outputValue = screen.getByTestId('output');
+  expect(outputValue.innerHTML).toBe('Sum is: 6')
+});
