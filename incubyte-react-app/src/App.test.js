fc2812754bee7eab379dbbad20ed5bbf653b0432
Arun Sharma <nuttygeek.arun@gmail.com>
2025-03-10 18:38:55 +0530

    test case for saperator for both \\n and , combined in a string

--- incubyte-react-app/src/App.test.js
+++ incubyte-react-app/src/App.test.js
@@ -67,3 +67,18 @@ test('should render sum when the string values are separated by \\n ', () => {
   const outputValue = screen.getByTestId('output');
   expect(outputValue.innerHTML).toBe('Sum is: 75')
 });
+
+test('should render sum when the string values are separated by \\n and , both ', () => {
+  render(<App />);
+  // getting input element
+  const inputWrapper = screen.getByTestId("input");
+  const inputElement = inputWrapper.querySelector("input");
+  // providing value to input element
+  fireEvent.change(inputElement, { target: { value: "15\\n25,35\\n67,90" } });
+  // getting calculate button
+  const calculateButton = screen.getByTestId('calculate');
+  // triggering click on calculate button
+  fireEvent.click(calculateButton);
+  const outputValue = screen.getByTestId('output');
+  expect(outputValue.innerHTML).toBe('Sum is: 232')
+});
